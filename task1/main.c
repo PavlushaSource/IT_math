c264e80bd7374a877e59b1db49b89f57bd8ce338
PavlushaSource <pavlushkin.dima1785@gmail.com>
2024-03-18 02:16:21 +0300

    refactor: Change algo output

--- task1/main.c
+++ task1/main.c
@@ -8,13 +8,15 @@
 #include <stdlib.h>
 
 const int NUM_THREADS[] = {1, 4, 8, 12};
-const int NET_SIZE = 500;
-const int BLOCK_SIZE = 64;
-const double EPS = 0.1;
+const int NET_SIZE[] = {100, 300, 500, 1000};
+const int BLOCK_SIZES[] = {1, 8, 16, 32, 64};
+
+const double EPS = 0.01;
 
 typedef struct net {
     double h;
-    int n;
+    int NS;
+    int BS;
 
     double **f;
     double **u;
@@ -22,12 +24,12 @@ typedef struct net {
 
 typedef double (*fun_xy)(double, double);
 
-double d_book_example(double x, double y) {
-    return 6000 * x + 12000 * y;
+double d_f(double x, double y) {
+    return 0;
 }
 
-double book_example(double x, double y) {
-    return 1000 * x * x * x + 2000 * y * y * y;
+double input_f(double x, double y) {
+    return 52;
 }
 
 double **create_double_2d_arr(size_t sz) {
@@ -47,13 +49,13 @@ static double min(double a, double b) { return a < b ? a : b; }
 
 static double max(double a, double b) { return a > b ? a : b; }
 
-net_t *create_net(int n, fun_xy g, fun_xy f) {
+net_t *create_net(int n, fun_xy g, fun_xy f, int BS) {
     net_t *my_net = malloc(sizeof(*my_net));
-    my_net->n = n;
+    my_net->NS = n;
     my_net->h = 1.0 / (n + 1);
     my_net->u = create_double_2d_arr(n + 2);
     my_net->f = create_double_2d_arr(n + 2);
-
+    my_net->BS = BS;
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= n; j++) {
             my_net->f[i][j] = f(i * my_net->h, j * my_net->h);
@@ -70,12 +72,12 @@ net_t *create_net(int n, fun_xy g, fun_xy f) {
 }
 
 double approximate_block(net_t *my_net, int i, int j) {
-    int li = 1 + i * BLOCK_SIZE;
-    int lj = 1 + j * BLOCK_SIZE;
+    int li = 1 + i * my_net->BS;
+    int lj = 1 + j * my_net->BS;
 
     double dmax = 0;
-    for (int ii = li; ii <= min(li + BLOCK_SIZE - 1, my_net->n); ii++) {
-        for (int jj = lj; jj <= min(lj + BLOCK_SIZE - 1, my_net->n); jj++) {
+    for (int ii = li; ii <= min(li + my_net->BS - 1, my_net->NS); ii++) {
+        for (int jj = lj; jj <= min(lj + my_net->BS - 1, my_net->NS); jj++) {
             double temp = my_net->u[ii][jj];
             my_net->u[ii][jj] = 0.25 * (my_net->u[ii - 1][jj] + my_net->u[ii][jj - 1] + my_net->u[ii + 1][jj] +
                                         my_net->u[ii][jj + 1] - my_net->h * my_net->h * my_net->f[ii][jj]);
@@ -89,7 +91,7 @@ double approximate_block(net_t *my_net, int i, int j) {
 
 size_t approximate(net_t *my_net) {
     size_t iter = 0;
-    int num_block = my_net->n / BLOCK_SIZE + (my_net->n % BLOCK_SIZE != 0);
+    int num_block = my_net->NS / my_net->BS + (my_net->NS % my_net->BS != 0);
     double dmax = 0;
     double *dm = calloc(num_block, sizeof(*dm));
 
@@ -129,33 +131,66 @@ size_t approximate(net_t *my_net) {
     return iter;
 }
 
-int main() {
+FILE *create_resulting_file(char *name) {
+    FILE *file = fopen(name, "w");
+
     for (int i = 0; i < sizeof(NUM_THREADS) / sizeof(NUM_THREADS[0]); ++i) {
-        omp_set_num_threads(NUM_THREADS[i]);
-        net_t *my_net = create_net(NET_SIZE, book_example, d_book_example);
-        double t_start = omp_get_wtime();
-        size_t count_iter = approximate(my_net);
-        double t_end = omp_get_wtime();
-
-        double max_e = -INFINITY;
-        double min_e = INFINITY;
-        double h = my_net->h;
-        printf("###################### RESULT FOR %d THREADS ######################\n", NUM_THREADS[i]);
-        printf("TIME: %f\n", t_end - t_start);
-        printf("COUNT OF ITERATION: %zu\n", count_iter);
-        for (int i = 1; i <= NET_SIZE; i++) {
-            for (int j = 1; j <= NET_SIZE; j++) {
-                max_e = max(max_e, fabs(book_example(i * h, j * h) - my_net->u[i][j]));
-                min_e = min(min_e, fabs(book_example(i * h, j * h) - my_net->u[i][j]));
-            }
-        }
-        printf("AVERAGE ERROR: %f\n", max_e / (NET_SIZE * NET_SIZE));
-        printf("MAX ERROR: %f\n", max_e);
-        printf("MIN ERROR: %f\n", min_e);
-
-        free_double_2d_arr(my_net->u, my_net->n + 2);
-        free_double_2d_arr(my_net->f, my_net->n + 2);
-        free(my_net);
+        fprintf(file, "%d ", NUM_THREADS[i]);
     }
+    fprintf(file, "\n");
+
+    for (int i = 0; i < sizeof(NET_SIZE) / sizeof(NET_SIZE[0]); ++i) {
+        fprintf(file, "%d ", NET_SIZE[i]);
+    }
+
+    fprintf(file, "\n");
+
+    for (int i = 0; i < sizeof(BLOCK_SIZES) / sizeof(BLOCK_SIZES[0]); ++i) {
+        fprintf(file, "%d ", BLOCK_SIZES[i]);
+    }
+    fprintf(file, "\n");
+    return file;
+}
+
+int main() {
+    FILE *file = create_resulting_file("result_function_1_test.txt");
+    for (int i = 0; i < sizeof(NUM_THREADS) / sizeof(NUM_THREADS[0]); ++i) {
+        for (int d = 0; d < sizeof(BLOCK_SIZES) / sizeof(BLOCK_SIZES[0]); ++d) {
+            fprintf(file, "| $blockSize = %d$ |", BLOCK_SIZES[d]);
+            for (int l = 0; l < sizeof(NET_SIZE) / sizeof(NET_SIZE[0]); ++l) {
+                omp_set_num_threads(NUM_THREADS[i]);
+                net_t *my_net = create_net(NET_SIZE[l], input_f, d_f, BLOCK_SIZES[d]);
+                double t_start = omp_get_wtime();
+                size_t count_iter = approximate(my_net);
+
+                double t_end = omp_get_wtime();
+
+                double max_e = -INFINITY;
+                double min_e = INFINITY;
+                double h = my_net->h;
+                printf("################# RESULT FOR %d THREADS, %d BS, %d NS  #################\n", NUM_THREADS[i],
+                       BLOCK_SIZES[d], NET_SIZE[l]);
+                printf("TIME: %.6f\n", t_end - t_start);
+                fprintf(file, " $%f$ |", t_end - t_start);
+//                printf("COUNT OF ITERATION: %zu\n", count_iter);
+//                for (int i = 1; i <= NET_SIZE[l]; i++) {
+//                    for (int j = 1; j <= NET_SIZE[l]; j++) {
+//                        max_e = max(max_e, fabs(input_f(i * h, j * h) - my_net->u[i][j]));
+//                        min_e = min(min_e, fabs(input_f(i * h, j * h) - my_net->u[i][j]));
+//                    }
+//                }
+//                printf("AVERAGE ERROR: %f\n", max_e / (NET_SIZE[l] * NET_SIZE[l]));
+//                printf("MAX ERROR: %f\n", max_e);
+//                printf("MIN ERROR: %f\n", min_e);
+
+                free_double_2d_arr(my_net->u, my_net->NS + 2);
+                free_double_2d_arr(my_net->f, my_net->NS + 2);
+                free(my_net);
+            }
+            fprintf(file, "\n");
+        }
+        fprintf(file, "\n");
+    }
+    fclose(file);
     return 0;
 }
